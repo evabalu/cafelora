63d85d18e1af56ba68925e85884ae8dfe08ad1c5
evabalu <baluchova.eva@gmail.com>
2023-06-15 19:38:32 +0200

    split to componets

---
+++ src/components/Banner/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+export const Banner = () => {
+  const element = document.createElement ('banner');
+  element.innerHTML = `
+    <section class="banner">
+    <div class="container">
+      <div class="banner__content">
+        <h1>Přijď to ochutnat!</h1>
+        <p>Nabízíme kvalitní kávu, útulné prostředí a perfektní servis.</p>
+      </div>
+    </div>
+  </section>
+    `;
+
+  return element;
+};
---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import './style.css';
+
+export const Header = () => {
+  const element = document.createElement ('header');
+  element.innerHTML = `
+    <header>
+    <div class="header__content container">
+      <div class="site-logo"></div>
+
+      <div class="navigation">
+        <button class="nav-btn"></button>
+        <nav class="rollout-nav nav-closed">
+          <a href="#home">domů</a>
+          <a href="#menu">menu</a>
+          <a href="#gallery">galerie</a>
+          <a href="#contact">kontakt</a>
+        </nav>
+      </div>
+
+    </div>
+  </header>
+    `;
+
+  return element;
+};
